a92a3cb59f890a2aac8abbafcf192876f0b7f8c5
josungryong <josung50@naver.com>
2017-10-24 21:27:45 +0900

    midi to sheet complete, sample : loverbattery

--- bin/readme.txt
+++ bin/readme.txt
@@ -5,3 +5,8 @@ image
 note_images , note_images_both
 * 한 손 연주용 , 두 손 연주용 악보입니다.
 * 두 폴더는 src/server/note/ 에 위치하여야 합니다.
+
+sheet.exe
+* 최상위 레벨의 sheet/에 존재해야 합니다.
+* ex) /var/www/html/sheet
+* sheet/song에서 불러들인 midi는 악보로 변환되어 sheet/ 에 저장됩니다.
--- src/server/note/hard/lovebattery.php
+++ src/server/note/hard/lovebattery.php
@@ -1,45 +1,85 @@
-<script>popup=window.open("http://10.30.112.19/lovebattery.php",'player','top=500,left=800,width=10,height=10,status=0,toolbar=0,location=0,menubar=0,resizable=0,scrollbars=0');
-</script>
 <?php
 session_start();
-$_SESSION['play_song'] = 'LOVEBATTERY';
+$_SESSION['play_song'] = 'LOVERBATTERY';
+?>
+
+<?php
+function CheckSheetExist($fileName) {
+    chdir('../../sheet/sheetimg');
+    //if(file_exists(_SESSION["play_song"]+ '_1.png'))
+    if(file_exists($fileName))
+        return true;
+    else
+        return false;
+}
+
+function CreateSheet() {
+    chdir('../../sheet');
+    //echo getcwd();
+    $commend = "mono sheet.exe songs/" . $_SESSION['play_song'] . ".mid" . " " . $_SESSION['play_song'];
+    //echo $commend;
+    exec($commend);
+}
+
+function SheetCount() {
+    $index = 1;
+    while(file_exists($_SESSION['play_song']. "_" . $index . ".png")) {
+        $index++;
+    }
+    return $index - 1;
+}
+?>
+
+<?php
+    // 현재 경로 -> php파일이 있는 경로
+    // sheet.exe가 있는 경로로 이동해야 한다.
+    chdir('../../sheet');
+    //exec("mono sheet.exe songs/LOVERBATTERY.mid testmid", $output, $error);
+    if(!CheckSheetExist($_SESSION['play_song'])) {
+        CreateSheet();
+    }
+    //echo SheetCount(); 결과 : 5 , 실제 악보수는 4
 ?>
 
 <html>
 <head>
-	<title>lovebattery</title> 
+    <title>loverbattery</title>
 </head>
-	
-	<body>
-		<br/><br/>
-		<img src="../note_images/lovebattery1.png" id="mainImage">
-		
-		<script>
-			// slide-show
-			var myImage=document.getElementById("mainImage");
-			var imageArray=["../note_images/lovebattery2.png",
-							"../note_images/lovebattery3.png" ];
-			
-			var imageIndex=0;
-			
-			function changeImage(){
-				myImage.setAttribute("src",imageArray[imageIndex]);
-				imageIndex++;
-				if(imageIndex==imageArray.length){
-					clearInterval(refreshIntervalId);
-				}
-			}
-			var refreshIntervalId = setInterval(changeImage,10000);	// 첫 줄 9초 
 
-		</script> 
-		<a href="http://10.30.112.19/end.php"><img src='/image/home.png'></a>
-		<meta http-equiv='refresh' content='33;url=http://35.161.154.86/score/score_load.php'>
+<body>
+<br/><br/>
 
-	</body>
+<!-- php 현재 경로 : /var/www/html/sheet -->
+<!-- html 의 경로는 그대로 /var/www/html/note/hard -->
+
+<img src="../../sheet/LOVERBATTERY_1.png" id="mainImage">
+
+<script>
+    // slide-show
+    var myImage=document.getElementById("mainImage");
+    var theNumberOfSheet =  <?=SheetCount()?>;
+
+    var imageArray = new Array();
+    for(var i=0; i<theNumberOfSheet; i++)
+        imageArray[i] = "../../sheet/LOVERBATTERY_" + parseInt(i+1) + ".png";
+        //var imageArray=["../note_images/lovebattery2.png",
+        //"../note_images/lovebattery3.png" ];
+
+    var imageIndex=0;
+
+    function ChangeImage(){
+        myImage.setAttribute("src",imageArray[imageIndex]);
+        imageIndex++;
+        if(imageIndex==imageArray.length){
+            clearInterval(refreshIntervalId);
+        }
+    }
+    var refreshIntervalId = setInterval(ChangeImage,5000);	// 첫 줄 9초
+
+</script>
+<a href="http://10.30.112.19/end.php"><img src='/image/home.png'></a>
+<meta http-equiv='refresh' content='33;url=http://35.161.154.86/score/score_load.php'>
+
+</body>
 </html>
-
-
-
-
-
 
