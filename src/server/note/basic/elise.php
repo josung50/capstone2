af381d5bc3e5d2efe03837432bede263354b0e67
Jo SungRyong <josung50@naver.com>
2017-11-19 14:43:00 +0900

    refactoring2

--- src/server/note/basic/elise.php
+++ src/server/note/basic/elise.php
@@ -1,9 +1,9 @@
 <?php
-    include_once ("../loadSheetFunction.php");
+    include_once ("../SheetFunction.php");
 ?>
 
 <script>
-    popup=window.open("http://10.30.112.19/elise.php",'player','top=500,left=800,width=10,height=10,status=0,toolbar=0,location=0,menubar=0,resizable=0,scrollbars=0');
+    popup=window.open("http://10.42.0.187/school.php",'player','top=500,left=800,width=10,height=10,status=0,toolbar=0,location=0,menubar=0,resizable=0,scrollbars=0');
 </script>
 
 <?php
@@ -22,32 +22,15 @@
         <title>Elise</title>
     </head>
 	
-	<body>
+	<body onload = "ScrollAuto();">
 		<br/><br/>
-		<img src="../../sheet/ELISE_1.png" id="mainImage">
-		
-		<script>
-			// slide-show
-			var myImage=document.getElementById("mainImage");
-            var theNumberOfSheet =  <?=SheetCount($_SESSION['play_song'])?>;
+        <script>
+            // slide-show
+            var theNumberOfSheet =  <?=CountSheet($_SESSION['play_song'])?>;
+            console.log("the Number of Sheet : " + theNumberOfSheet);
 
-            var imageArray = new Array();
-            for(var i = 0; i < theNumberOfSheet; i++)
-                imageArray[i] = "../../sheet/ELISE_" + parseInt(i + 1) + ".png";
-			var imageIndex=0;
-			
-			function changeImage() {
-				myImage.setAttribute("src", imageArray[imageIndex]);
-				imageIndex++;
-				if(imageIndex==imageArray.length)
-					clearInterval(refreshIntervalId);
-			}
-			var refreshIntervalId = setInterval(changeImage, 21000);
-
-		</script>
-
-		<meta http-equiv='refresh' content='28;url=http://35.161.154.86/score/score_load.php'>
-
+            CreateImgTag(theNumberOfSheet, 'ELISE');
+        </script>
 	</body>
 
 </html>
--- src/server/note/basic/sun.php
+++ src/server/note/basic/sun.php
@@ -1,9 +1,9 @@
 <?php
-    include_once ("../loadSheetFunction.php");
+    include_once ("../SheetFunction.php");
 ?>
 
 <script>
-    popup=window.open("http://10.30.112.19/sun.php",'player','top=500,left=800,width=10,height=10,status=0,toolbar=0,location=0,menubar=0,resizable=0,scrollbars=0');
+    popup=window.open("http://10.42.0.187/school.php",'player','top=500,left=800,width=10,height=10,status=0,toolbar=0,location=0,menubar=0,resizable=0,scrollbars=0');
 </script>
 
 <?php
@@ -24,31 +24,14 @@
         <title>Sun</title>
     </head>
 	
-	<body>		
-		<img src="../../sheet/SUN_1.png" id="mainImage">
-		
-		<script>
-			// slide-show
-			var myImage=document.getElementById("mainImage");
-            var theNumberOfSheet =  <?=SheetCount($_SESSION['play_song'])?>;
+	<body onload = "ScrollAuto();">
+        <br/><br/>
+        <script>
+            // slide-show
+            var theNumberOfSheet =  <?=CountSheet($_SESSION['play_song'])?>;
+            console.log("the Number of Sheet : " + theNumberOfSheet);
 
-            var imageArray = new Array();
-            for(var i = 0; i < theNumberOfSheet; i++)
-                imageArray[i] = "../../sheet/SUN_" + parseInt(i + 1) + ".png";
-
-            var imageIndex = 0;
-			
-			function changeImage() {
-				myImage.setAttribute("src", imageArray[imageIndex]);
-				imageIndex++;
-				if(imageIndex==imageArray.length)
-					clearInterval(refreshIntervalId);
-			}
-			var refreshIntervalId = setInterval(changeImage, 19000);		// 19초
-
-		</script>
-
-		<meta http-equiv='refresh' content='39;url=http://35.161.154.86/score/score_load.php'>
-		
+            CreateImgTag(theNumberOfSheet, 'SUN');
+        </script>
 	</body>
 </html>
