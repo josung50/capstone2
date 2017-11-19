4aacf4b67f93e861a12ef69f483943077bbf7284
Jo SungRyong <josung50@naver.com>
2017-11-19 14:44:16 +0900

    refactoring2

--- src/server/note/hard/pirate.php
+++ src/server/note/hard/pirate.php
@@ -1,9 +1,9 @@
 <?php
-    include_once ("../loadSheetFunction.php");
+    include_once ("../SheetFunction.php");
 ?>
 
 <script>
-    popup=window.open("http://10.30.112.19/pirate.php",'player','top=500,left=800,width=10,height=10,status=0,toolbar=0,location=0,menubar=0,resizable=0,scrollbars=0');
+    popup=window.open("http://10.42.0.187/school.php",'player','top=500,left=800,width=10,height=10,status=0,toolbar=0,location=0,menubar=0,resizable=0,scrollbars=0');
 </script>
 
 <?php
@@ -24,44 +24,14 @@
         <title>Pirate</title>
     </head>
 
-	<body>
-		<!--a href="../basic.php">again select song</a-->
-					
-		<img src="../../sheet/PIRATE_1.png" id="mainImage">
-		
-		<script>
-			// slide-show
-			var myImage=document.getElementById("mainImage");
-            var theNumberOfSheet =  <?=SheetCount($_SESSION['play_song'])?>;
-
-            var imageArray = new Array();
-            for(var i = 0; i < theNumberOfSheet; i++)
-                imageArray[i] = "../../sheet/PIRATE_" + parseInt(i + 1) + ".png";
-			var imageIndex=0;
-			
-			function changeImage() {
-				myImage.setAttribute("src", imageArray[imageIndex]);
-				imageIndex++;
-				if(imageIndex == imageArray.length)
-					clearInterval(refreshIntervalId);
-				else if(imageIndex == 0) {
-					clearInterval(refreshIntervalId);
-					refreshIntervalId = setInterval(changeImage, 11000); // 2 : 9.6초
-				}
-				else if(imageIndex == 1) {
-					clearInterval(refreshIntervalId);
-					refreshIntervalId = setInterval(changeImage, 10050); // 2 : 9.6초
-				}
-				else if(imageIndex >= 2) {
-					clearInterval(refreshIntervalId);
-					refreshIntervalId = setInterval(changeImage, 11000); // 5 : 8초
-				}
-			}
-			var refreshIntervalId = setInterval(changeImage, 11050);	// 첫번째 줄 8초
-
-		</script>
-
-		<meta http-equiv='refresh' content='55;url=http://35.161.154.86/score/score_load.php'>
+	<body onload = "ScrollAuto();">
+        <br/><br/>
+        <script>
+            // slide-show
+            var theNumberOfSheet =  <?=CountSheet($_SESSION['play_song'])?>;
+            console.log("the Number of Sheet : " + theNumberOfSheet);
+            CreateImgTag(theNumberOfSheet, 'PIRATE');
+        </script>
 	
 	</body>
 
