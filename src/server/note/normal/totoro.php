d824d7c8ef13b98fc2cde916bec4fee3c4e93bfe
leedh8036 <leedh8036@hanmail.net>
2017-11-30 18:18:28 +0900

    final refactoring multi access from web to rasp - totoro.php
    final refactoring multi access from web to rasp

--- src/server/note/normal/totoro.php
+++ src/server/note/normal/totoro.php
@@ -1,44 +1,49 @@
 <?php
-    include_once ("../loadSheetFunction.php");
+include_once ("../SheetFunction.php");
 ?>
 
 <script>
-    popup=window.open("http://10.42.0.187/school.php",'player','top=500,left=800,width=10,height=10,status=0,toolbar=0,location=0,menubar=0,resizable=0,scrollbars=0');
+    //popup=window.open("http://10.30.112.19/school.php",'player','top=500,left=800,width=10,height=10,status=0,toolbar=0,location=0,menubar=0,resizable=0,scrollbars=0');
+    //popup=window.open("http://10.42.0.187/totoro.php",'player','top=500,left=800,width=10,height=10,status=0,toolbar=0,location=0,menubar=0,resizable=0,scrollbars=0');
 </script>
 
 <?php
-    session_start();
-    $_SESSION['play_song'] = 'TOTORO';
+session_start();
+$_SESSION['play_song'] = 'TOTORO';
+
+$user_ip[] = $_SERVER["REMOTE_ADDR"];
+//echo $user_ip[0];
+
+$rasp_songfile = 'totoro.php';
 ?>
+<script>
+
+    popup=window.open("http://<?php echo $user_ip[0];?>/<?php echo $rasp_songfile;?>",'player','top=500,left=800,width=10,height=10,status=0,toolbar=0,location=0,menubar=0,resizable=0,scrollbars=0');
+</script>
 
 <?php
-    // 현재 경로 -> php파일이 있는 경로
-    // sheet.exe가 있는 경로로 이동해야 한다.
-    chdir('../../sheet');
-    if(!CheckSheetExist($_SESSION['play_song']))
-        CreateSheet($_SESSION['play_song']);
+// 현재 경로 -> php파일이 있는 경로
+// sheet.exe가 있는 경로로 이동해야 한다.
+chdir('../../sheet');
+if(!CheckSheetExist($_SESSION['play_song']))
+    CreateSheet($_SESSION['play_song']);
 ?>
 
 <html>
-    <head>
-        <title>totoro</title>
-    </head>
-	
-	<body onload = "ScrollAuto();">
-        <br/><br/>
-        <script>
-            // slide-show
-            var theNumberOfSheet =  <?=CountSheet($_SESSION['play_song'])?>;
-            console.log("the Number of Sheet : " + theNumberOfSheet);
+<head>
+    <title>Totoro</title>
+</head>
 
-            CreateImgTag(theNumberOfSheet, 'TOTORO');
-        </script>
-	</body>
+<body onload = "ScrollAuto();">
+<br/><br/>
+<script>
+    // slide-show
+    var theNumberOfSheet =  <?=CountSheet($_SESSION['play_song'])?>;
+    console.log("the Number of Sheet : " + theNumberOfSheet);
 
+    CreateImgTag(theNumberOfSheet, 'TOTORO');
+</script>
+</body>
 </html>
-
-
-
-
 
 
