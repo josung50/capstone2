b6f790a8a5e31bf428437f98b39e95f5de50aa4c
leedh8036 <leedh8036@hanmail.net>
2017-11-30 18:12:53 +0900

    final refactoring multi access from web to rasp - schoo.php
    final refactoring multi access from web to rasp

--- src/server/note/basic/school.php
+++ src/server/note/basic/school.php
@@ -4,14 +4,22 @@
 
 <script>
     //popup=window.open("http://10.30.112.19/school.php",'player','top=500,left=800,width=10,height=10,status=0,toolbar=0,location=0,menubar=0,resizable=0,scrollbars=0');
-    popup=window.open("http://10.42.0.187/school.php",'player','top=500,left=800,width=10,height=10,status=0,toolbar=0,location=0,menubar=0,resizable=0,scrollbars=0');
+    //popup=window.open("http://10.42.0.187/school.php",'player','top=500,left=800,width=10,height=10,status=0,toolbar=0,location=0,menubar=0,resizable=0,scrollbars=0');
 </script>
 
 <?php
     session_start();
     $_SESSION['play_song'] = 'SCHOOL';
-?>
+	
+	$user_ip[] = $_SERVER["REMOTE_ADDR"];
+	//echo $user_ip[0];
 
+	$rasp_songfile = 'school.php';
+?>
+<script>
+
+    popup=window.open("http://<?php echo $user_ip[0];?>/<?php echo $rasp_songfile;?>",'player','top=500,left=800,width=10,height=10,status=0,toolbar=0,location=0,menubar=0,resizable=0,scrollbars=0');
+</script>
 <?php
     // 현재 경로 -> php파일이 있는 경로
     // sheet.exe가 있는 경로로 이동해야 한다.
