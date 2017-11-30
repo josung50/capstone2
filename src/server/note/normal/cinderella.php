9516af47f14665d198a2b5cac373c5f21e3a9c07
leedh8036 <leedh8036@hanmail.net>
2017-11-30 18:17:23 +0900

    final refactoring multi access from web to rasp - cinderella.php
    final refactoring multi access from web to rasp

--- src/server/note/normal/cinderella.php
+++ src/server/note/normal/cinderella.php
@@ -1,45 +1,52 @@
 <?php
-    include_once ("../SheetFunction.php");
+include_once ("../SheetFunction.php");
 ?>
 
-<?php
-	$user_ip[] = $_SERVER["REMOTE_ADDR"];
-	echo '$user_ip : '.$user_ip.'<br>';
-    session_start();
-    $_SESSION['play_song'] = 'CINDERELLA';
-?>
 <script>
-    $user_ip[] = $_SERVER["REMOTE_ADDR"];
-    popup=window.open("http://<?php echo $user_ip[0];?>",'player','top=500,left=800,width=10,height=10,status=0,toolbar=0,location=0,menubar=0,resizable=0,scrollbars=0');
+    //popup=window.open("http://10.30.112.19/school.php",'player','top=500,left=800,width=10,height=10,status=0,toolbar=0,location=0,menubar=0,resizable=0,scrollbars=0');
+    //popup=window.open("http://10.42.0.187/cinderella.php",'player','top=500,left=800,width=10,height=10,status=0,toolbar=0,location=0,menubar=0,resizable=0,scrollbars=0');
 </script>
 
 <?php
-    // 현재 경로 -> php파일이 있는 경로
-    // sheet.exe가 있는 경로로 이동해야 한다.
-    chdir('../../sheet');
-    if(!CheckSheetExist($_SESSION['play_song']))
-        CreateSheet($_SESSION['play_song']);
+
+$user_ip[] = $_SERVER["REMOTE_ADDR"];
+//echo $user_ip[0];
+
+
+session_start();
+$_SESSION['play_song'] = 'CINDERELLA';
+
+$rasp_songfile = 'cinderella.php';
+?>
+
+<script>
+
+    popup=window.open("http://<?php echo $user_ip[0];?>/<?php echo $rasp_songfile;?>",'player','top=500,left=800,width=10,height=10,status=0,toolbar=0,location=0,menubar=0,resizable=0,scrollbars=0');
+</script>
+
+<?php
+// 현재 경로 -> php파일이 있는 경로
+// sheet.exe가 있는 경로로 이동해야 한다.
+chdir('../../sheet');
+if(!CheckSheetExist($_SESSION['play_song']))
+    CreateSheet($_SESSION['play_song']);
 ?>
 
 <html>
-    <head>
-        <title>Cinderella</title>
-    </head>
-	
-	<body onload = "ScrollAuto();">
-		<br/><br/>
-        <script>
-            // slide-show
-            var theNumberOfSheet =  <?=CountSheet($_SESSION['play_song'])?>;
-            console.log("the Number of Sheet : " + theNumberOfSheet);
+<head>
+    <title>Cinderella</title>
+</head>
 
-            CreateImgTag(theNumberOfSheet, 'CINDERELLA');
-        </script>
-	</body>
+<body onload = "ScrollAuto();">
+<br/><br/>
+<script>
+    // slide-show
+    var theNumberOfSheet =  <?=CountSheet($_SESSION['play_song'])?>;
+    console.log("the Number of Sheet : " + theNumberOfSheet);
+
+    CreateImgTag(theNumberOfSheet, 'CINDERELLA');
+</script>
+</body>
 </html>
-
-
-
-
 
 
